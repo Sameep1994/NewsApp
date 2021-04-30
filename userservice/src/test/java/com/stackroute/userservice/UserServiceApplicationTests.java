2709bb350259d0ff5e9002fe8c461b1b6df2471e
Sameep1994 <63553538+Sameep1994@users.noreply.github.com>
2021-05-01 02:43:54 +0530

    test

--- userservice/src/test/java/com/stackroute/userservice/UserServiceApplicationTests.java
+++ userservice/src/test/java/com/stackroute/userservice/UserServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package com.stackroute.userservice;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,9 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class UserServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
 
